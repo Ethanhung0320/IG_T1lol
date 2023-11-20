9beaa0e7c443cbd676a076aa5274f79814c5cb3e
Ethan <q40131125@gmail.com>
2023-11-20 16:07:23 +0800

    Initial Commit

---
+++ IG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG
+//
+//  Created by Ethan Hung on 2023/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
